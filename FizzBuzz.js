a76660a611e8450ed40ff529f75c8955431eea37
BrandonSRT <brandon.se2009@gmail.com>
2021-09-10 15:22:22 -0600

    Updating == to ===

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,11 +1,11 @@
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
     } else {
-        if (i % 3 == 0) {
+        if (i % 3 === 0) {
             console.log("Fizz");
         } else {
-            if (i % 5 == 0) {
+            if (i % 5 === 0) {
                 console.log("Buzz");
             } else {
                 console.log(i);
@@ -19,13 +19,13 @@ for (let i = 1; i <= 100; i++) {
 function FizzBuzz(limit){
 
     for (let i = 1; i <= limit; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
+        if (i % 3 === 0 && i % 5 === 0) {
             console.log("FizzBuzz");
         } else {
-            if (i % 3 == 0) {
+            if (i % 3 === 0) {
                 console.log("Fizz");
             } else {
-                if (i % 5 == 0) {
+                if (i % 5 === 0) {
                     console.log("Buzz");
                 } else {
                     console.log(i);
